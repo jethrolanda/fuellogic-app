d386272d502b41e81548491ff816ba28a8b01f4e
Jethro Landa <jemaq_lan@hotmail.com>
2025-01-10 20:28:04 +0800

    bottom nav

---
+++ blocks/build/loading - Copy/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => 'b9829d45aefe5f63486a');
---
+++ blocks/build/menu/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => 'f1ea8bd9466b91e1518b');
